bb12e9dc66a7c41aa21f43471524fe83d7eb71f6
JisuPark <53842434+haeju20@users.noreply.github.com>
2020-12-10 20:35:22 +0900

    Add files via upload
    search result 
    - 검색 결과를 테이블 형식으로 표현(php table 해결)
    - 아이콘 삽입(php img tag 사용)

---
+++ search.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset = "utf-8">
+  <meta name = "description" content = "search result page">
+  <title>$input</title> <!--사용자가 입력한 검색어를 타이틀로-->
+  <link rel = "stylesheet" href = "search.css">
+  <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+  <div id = "wrapper">
+    <header id = "main_header">
+      <a class = "top_menu" href = "main.html" target = "_top">SIGN UP</a> <!--signup페이지로 연결-->
+      <a class = "top_menu" href = "main.html" target = "_top">SIGN IN</a> <!--signin페이지로 연결-->
+    </header>
+  </div>
+  <form class = 'search'>
+    <div id = "container">
+      <div id = "logo_img">
+        <a href = "main.html" target = "_top">
+          <img src = "logo2_removebg.png" border = "0">
+        </a>
+      </div>
+      <div id = "main_search">
+        <input onkeyup = "filter()" type = "text" class = "search_word" name = "keyword" autocomplete = "off" placeholder = "Type Channel Name">
+        <button onclick = "search_youtube()" type = "button" class = "search_btn" name = "click_btn" value = "">
+          <i class = "fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+  </form>
+ 
+  <?php
+  //mysql 접속 계정 정보 설정
+  $mysql_host = '127.0.0.1';
+  $mysql_user = 'root';
+  $mysql_password = '';
+  $mysql_db = 'youtube_info';
+  //connetc 설정(host,user,password)
+  $conn = mysqli_connect($mysql_host,$mysql_user,$mysql_password,$mysql_db)or die("fail");
+
+  //쿼리문 작성
+  echo "<tr><th>$_POST[keyword]";
+  echo "의 검색결과";
+
+  $input = $_POST['keyword'];
+  $query = "SELECT * FROM channels WHERE name LIKE '%$input%'";
+  //쿼리보내고 결과를 변수에 저장
+  $result = mysqli_query($conn, $query);
+ ?>
+
+  <div id = "table_style">
+    <table class = "search_result_table">
+      <thead class = "th">
+        <tr>
+          <th>Catergory</th>
+          <th>Channel Name</th>
+          <th>Info</th>
+          <th>Rating / Reveiws</th>
+        </tr>
+      </thead>
+      <tbody>
+    
+
+  <?php
+    
+    if (mysqli_num_rows($result) > 0) {
+      while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $row["category"]. "</td><td>" . $row["name"]. "</td><td>";
+        echo "<p>";
+        echo "<img src='img_views.png'/>". " ". $row["views"]."</p>";
+        echo "<p>";
+        echo "<img src='img_subs.png'/>"." ". $row["subscribers"]."</p>";
+        echo "<p>";
+        echo "<img src='img_videos.png'/>"." ". $row["videos"]."</p></td></tr>";
+      }
+    } else {
+      echo "No search result.";
+    }
+  ?>
+      </tbody>
+    </table>
+  </div> 
+
+  <script type="text/javascript" src="keyword_search.js"></script>
+
+</body>
+</html>
