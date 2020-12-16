9c9d9ef95aa81d9bcae8527447691fa0811fb3e0
JisuPark <53842434+haeju20@users.noreply.github.com>
2020-12-17 01:01:15 +0900

    Add files via upload

--- main.php
+++ main.php
@@ -6,7 +6,7 @@
   <meta charset = "utf-8">
   <meta name = "description" content = "main page">
   <title>Youties</title>
-  <link rel = "stylesheet" href = "main.css">
+  <link rel = "stylesheet" href = "main.css?after">
   <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 </head>
 <body>
@@ -21,10 +21,22 @@
     <img src = "logo2.png" border = "0">
   </div>
   <div>
+      <!--review 수, channel 수, member 수 db 연결-->
     <p id = "main_info">REVIEWS: &nbsp&nbspCHANNELS: &nbsp&nbspMEMBERS: &nbsp&nbspVISITORS: <?php echo $today_visit_count?>&nbsp&nbsp</p>
   </div>
+
+  <?php
+    $conn = mysqli_connect($mysql_host,$mysql_user,$mysql_password,'youtube_info')or die("fail");
+    $query = "SELECT * FROM channels WHERE name";
+    $result = mysqli_query($conn, $query);
+
+    $cnt_query = "SELECT count(0) FROM channels";
+
+
+
+  ?>
   <div id = "main_search">
-    <input type = "text" class = "search_word" name = "keyword" id = "keyword" autocomplete = "off" autofocusplaceholder = "Type Channel Name">
+    <input type = "text" class = "search_word" name = "keyword" id = "keyword" autocomplete = "off" autofocus placeholder = "Type Channel Name">
     <button type = "button" class = "search_btn" name = "click_btn" value = "">
       <i class = "fas fa-search"></i>
     </button>
@@ -33,7 +45,77 @@
     <ul class = "list-group" id = "list"></ul>
   </div>
 </form>
-<script type="text/javascript" src="main.js"></script>
+<script type="text/javascript" src="./main2.js">
+  const people = new Object();
+  for (var i in <?php 
+    if (mysqli_num_rows($result) > 0) { 
+      while($row = mysqli_fetch_assoc($result)) {
+        
+        echo $row["name"];
+        
+      }
+    } ?>) { //등록된 채널 수만큼 반복
+    people.name = <?php echo $result ?>; //객체 삽입
+  }
 
+const list = document.getElementById('list');
+
+function setList(group){
+  clearList();
+  for (const person of group) {
+    const item = document.createElement('li');
+    item.classList.add('list-group-item');
+    const text = document.createTextNode(person.name);
+    item.appendChild(text);
+    list.appendChild(item);
+  }
+  if(group.length === 0){
+    setNoResult();
+  }
+}
+
+function clearList(){
+  while(list.firstChild){
+    list.removeChild(list.firstChild);
+  }
+}
+
+function setNoResult() {
+  const item = document.createElement('li');
+  item.classList.add('list-group-item');
+  const text = document.createTextNode('No results found');
+  item.appendChild(text);
+  list.appendChild(item);
+}
+
+function computeRelevancy(name, searchTerm) {
+let value = name.trim().toLowerCase();
+  if (value === searchTerm) {
+    return 2;
+  } else if (value.startsWith(searchTerm)){
+    return 1;
+  } else if (value.includes(searchTerm)){
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+const searchInput = document.getElementById('keyword');
+searchInput.addEventListener('input', (event) => {
+  let value = event.target.value;
+  if (value && value.trim().length > 0) {
+    value = value.trim().toLowerCase();
+    setList(people.filter(person => {
+      return person.name.toLowerCase().includes(value);
+    }).sort((person1, person2) => {
+      return computeRelevancy(person2.name, value) - computeRelevancy(person1.name, value);
+    }));
+  } else {
+    clearList();
+  }
+})
+  
+</script>
 </body>
 </html>
